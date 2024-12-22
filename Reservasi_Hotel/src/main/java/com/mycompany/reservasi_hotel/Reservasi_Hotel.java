0a78108a1f40f216a7ad9c6449f89b0c5f0c6a8a
MasAris1 <aris.maulana.am57@gmail.com>
2024-12-22 04:03:42 -0800

    Add files via upload

---
+++ Reservasi_Hotel/src/main/java/com/mycompany/reservasi_hotel/Reservasi_Hotel.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reservasi_hotel;
+
+/**
+ *
+ * @author USER
+ */
+public class Reservasi_Hotel {
+
+    public static void main(String[] args) {
+        Frame_program_reservasi_hotel Laund = new Frame_program_reservasi_hotel();
+        Laund.setVisible(true);
+    }
+}
